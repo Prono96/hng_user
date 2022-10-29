6ec23fc26fbee471e727f6b433cac93198bb7496
Promise <promiseprince01@gmail.com>
2022-10-29 16:47:57 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json({extended: false}));
+
+const user = { 
+slackUsername: 'prinx96', 
+backend: true, 
+age: 26, 
+bio: 'I am Promise Prince, the coolest backend developer ever'
+}
+
+app.get('/home', (req, res) => {
+  res.status(200).json(user);
+});
+
+// App to listen for a server 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {console.log("Server running on port 5000");});
